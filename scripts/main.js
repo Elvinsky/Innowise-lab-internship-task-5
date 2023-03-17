b8b3e11c018228bd546228c3a65e3a6982756585
Elvinsky <mikhnevichnik@gmail.com>
2023-03-17 15:30:14 +0300

    UI and linter cinfigured

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+import { valuesArr, primaryInd, actionsInd } from './consts.js';
+const buttonEl = document.createElement('button');
+const gridContainer = document.getElementById('grid-container');
+
+const renderContainer = (size) => {
+  for (let i = 0; i < size; i++) {
+    const buttonCopy = buttonEl.cloneNode(true);
+    buttonCopy.innerHTML = valuesArr[i];
+    buttonCopy.classList.add('grid-btn');
+    if (primaryInd.includes(i))buttonCopy.classList.add('primary');
+    if (actionsInd.includes(i))buttonCopy.classList.add('actions');
+    if (i === 28) {
+      buttonCopy.disabled = true;
+      buttonCopy.innerHTML = 'Innowise Lab Task 4';
+      gridContainer.append(buttonCopy);
+    }
+    buttonCopy.addEventListener('click', () => {
+      buttonCopy.classList.add('clicked');
+      setTimeout(() => {
+        buttonCopy.classList.remove('clicked');
+      }, 200);
+    });
+    gridContainer.append(buttonCopy);
+  }
+};
+
+renderContainer(32);
